20cd53d6efe699f60787cf9e09579b300ecc68a0
github_ayns789 <joly.a.idf@gmail.com>
2024-01-09 18:01:42 +0100

    MailApplication > add sendMail method

--- src/main/java/com/aynsprojects/mail/MailApplication.java
+++ src/main/java/com/aynsprojects/mail/MailApplication.java
@@ -1,13 +1,36 @@
 package com.aynsprojects.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class MailApplication {
 
+	@Autowired
+	private final JavaMailSender emailSender;
+
+	protected MailApplication(JavaMailSender emailSender) {
+		this.emailSender = emailSender;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(MailApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MailApplication.class, args);
+		MailApplication mailApplication = context.getBean(MailApplication.class);
+		mailApplication.sendMail();
+	}
+
+	public void sendMail()  {
+		System.out.println("ON ENTRE DANS LA METHODE 'SENDMAIL()'");
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("joly.idf@outlook.fr");
+		message.setTo("joly.idf@outlook.fr");
+		message.setSubject("test sender best mail");
+		message.setText("on y arrive !");
+		emailSender.send(message);
 	}
 
 }
